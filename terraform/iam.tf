5a54ea0b70434edacb09f9bcc3f3e6d7201fcbce
Michael Uftring <michael.uftring@gmail.com>
2024-03-19 13:51:36 -0400

    add permissions on S3 buckets

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -141,12 +141,14 @@ data "aws_iam_policy_document" "ecs_task_iam_policy_document" {
 
     actions = [
       "s3:GetObject",
+      "s3:GetObjectAttributes",
       "s3:GetObjectVersion",
       "s3:GetObjectVersionAttributes",
       "s3:DeleteObject",
       "s3:DeleteObjectVersion",
       "s3:ListBucket",
       "s3:ListBucketVersions",
+      "s3:PutObject"
     ]
 
     resources = [
@@ -180,11 +182,14 @@ data "aws_iam_policy_document" "ecs_task_iam_policy_document" {
     effect  = "Allow"
     actions = [
       "s3:GetObject",
+      "s3:GetObjectAttributes",
       "s3:GetObjectVersion",
       "s3:GetObjectVersionAttributes",
       "s3:ListBucket",
       "s3:ListBucketVersions",
-      "s3:PutObject"
+      "s3:PutObject",
+      "s3:DeleteObject",
+      "s3:DeleteObjectVersion"
     ]
 
     resources = [
