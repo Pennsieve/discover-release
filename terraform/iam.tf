50ababb2eb4e2496e7c01a1d6b06df7990437503
John Frommeyer <johnfr@seas.upenn.edu>
2026-04-16 15:21:12 -0400

    Add *ObjectTagging permissions for relevant buckets. (#16)

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -144,6 +144,7 @@ data "aws_iam_policy_document" "ecs_task_iam_policy_document" {
       "s3:GetObjectAttributes",
       "s3:GetObjectVersion",
       "s3:GetObjectVersionAttributes",
+      "s3:GetObjectTagging",
       "s3:DeleteObject",
       "s3:DeleteObjectVersion",
       "s3:ListBucket",
@@ -192,6 +193,7 @@ data "aws_iam_policy_document" "ecs_task_iam_policy_document" {
       "s3:ListBucket",
       "s3:ListBucketVersions",
       "s3:PutObject",
+      "s3:PutObjectTagging",
       "s3:DeleteObject",
       "s3:DeleteObjectVersion"
     ]
